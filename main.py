c7af7b34faa92242884cc62afad950d70ac4431a
prajwalkamath <65080907+prajwalkamath@users.noreply.github.com>
2021-11-18 21:17:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#import findspark
+import time
+from pyspark import SparkConf,SparkContext,SQLContext
+from pyspark.streaming import StreamingContext
+import sys
+import requests
+#findspark.init()
+conf=SparkConf()
+conf.setAppName("Sentiment Analysis")
+sc=SparkContext(conf=conf)
+sqlContext=SQLContext(sc)
+ssc=StreamingContext(sc,5)
+ssc.checkpoint("checkpoint_Sentiment")
+dataStream=ssc.socketTextStream("localhost",6100)
+#print(type(dataStream))
+#df=sqlContext.read.json(sc.parallelize([dataStream]))
+#df.show(5)
+dataStream.pprint()
+ssc.start()    
+ssc.awaitTermination()
+ssc.stop()
