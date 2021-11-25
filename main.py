e19b13344a13a5965805f5e2c6e63dd7d75fce20
prajwalkamath <65080907+prajwalkamath@users.noreply.github.com>
2021-11-25 21:31:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+#import findspark
+import time
+from pyspark import SparkConf,SparkContext
+from pyspark.sql.types import StructType,StructField,StringType
+from pyspark.sql import SparkSession,SQLContext,Row
+from pyspark.streaming import StreamingContext
+import sys
+import json
+import requests
+
+#conf=SparkConf()
+#conf.setAppName("Sentiment Analysis")
+#spark = SparkSession.builder.config(conf=SparkConf()).getOrCreate()
+conf=SparkConf()
+conf.setAppName("Sentiment Analysis")
+sc = SparkContext(conf=conf)
+#sqlContext = SQLContext(sc)
+Senti_schema= StructType([       
+    StructField('Sentiment', StringType(), False),
+    StructField('Tweet', StringType(), False)
+])
+def create_df(rdd):
+	spark = SparkContext.getOrCreate()
+	sqlContext = SQLContext(spark)
+	records = rdd.collect()
+	lst=[]
+	#records.show()
+	
+	#dicts = [i for j in records for i in list(json.loads(j).values())]
+	c=0
+	for i in records:
+		for j in json.loads(i).values():
+			lst.append(j)
+	rowData = map(lambda x: Row(**x),lst) 
+	dfFromData3 = sqlContext.createDataFrame(rowData,Senti_schema)
+	dfFromData3.show()
+
+ssc = StreamingContext(sc,5)
+ssc.checkpoint("checkpoint_Sentiment")
+dataStream=ssc.socketTextStream("localhost",6100)
+dataStream.foreachRDD(create_df)
+#print(type(dataStream))
+#par = sc.parallelize(dataStream)
+#df = sqlContext.read.json(par)
+#df.show(5)
+#record = dataStream.flatMap(json.loads)
+#record.pprint()
+#record = dataStream.flatMap(lambda x:x.split('\n'))
+#record.pprint()
+#dataStream.pprint()
+#aDict = json.loads(dataStream)
+
+ssc.start()    
+ssc.awaitTermination()
+ssc.stop()
+
